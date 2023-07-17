ce76ea8fdb28c234f8eadbd7f121c3feff25f742
HemangVora <hemangvora2@gmail.com>
2023-07-18 05:03:43 +0530

    Admin routes changed

--- src/pages/api/addMarketPlace.js
+++ src/pages/api/addMarketPlace.js
@@ -1,6 +1,7 @@
 import { Marketplaces } from "@/models/MarketPlaces";
 import clientPromise from "../../../lib/mongodb";
 import connectMongo from "@/database/conn";
+import { NftDetails } from "@/models/NftDetails";
 
 export default async (req, res) => {
   try {
@@ -17,24 +18,43 @@ export default async (req, res) => {
       NFTDetails1,
       NFTDetails2,
     } = req.body;
-   await connectMongo().catch(err=>console.log(err))
-   let post = await Marketplaces.create({
-        marketPlaceName,
-        marketPlaceCategory,
-        marketPlaceReleaseDateTime,
-        NFTQuantity,
-        maxNFTPUser,
-        initialPrice,
-        adminCommision,
-        NFTDetails1, 
-        NFTDetails2,
-    }).then((resp) => {
-        console.log(resp);
-        console.log("New User Added");
-        
-      });
-      res.json(post);
+    await connectMongo().catch((err) => console.log(err));
 
+    let nftDetailsLen = NftDetails.find({}).length + 1;
+
+   let details=  await NftDetails.create([
+      {
+        id: nftDetailsLen,
+        marketPlaceName: marketPlaceName,
+        marketPlaceCategory: marketPlaceCategory,
+        name: NFTDetails1.name,
+        price: NFTDetails1.price,
+        change: Math.floor(Math.random() * 70) + 1,
+        ipfs: NFTDetails1.ipfs,
+      },
+      {
+        id: nftDetailsLen + 1,
+        marketPlaceName: marketPlaceName,
+        marketPlaceCategory: marketPlaceCategory,
+        name: NFTDetails2.name,
+        price: NFTDetails2.price,
+        change: Math.floor(Math.random() * 70) + 1,
+        ipfs: NFTDetails2.ipfs,
+      },
+    ]);
+    let post = await Marketplaces.create({
+      marketPlaceName,
+      marketPlaceCategory,
+      marketPlaceReleaseDateTime,
+      NFTQuantity,
+      maxNFTPUser,
+      initialPrice,
+      adminCommision, 
+    }).then((resp) => {
+      console.log(resp);
+      console.log("New User Added");
+    });
+    res.json(post);
   } catch (e) {
     console.error(e);
     throw new Error(e).message;
