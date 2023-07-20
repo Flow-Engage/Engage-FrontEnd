56cfc1463cb6b21f1b148769b562ddd9ce67044f
Noah Naizir <Itahand.naizir@gmail.com>
2023-07-20 13:40:58 -0500

    AddCategory

--- src/flow/actions.js
+++ src/flow/actions.js
@@ -10,17 +10,32 @@ fcl.config().put("accessNode.api", "https://testnet.onflow.org");
 // Scripts imports
 import { get_NFTs_metadata as getNFTsmetadataScript } from "./Scripts/get_NFTs_metadata";
 import { getPlatformData as getPlatformDataScript } from "./Scripts/get_platform_data";
+import { getCategoryData as getCategoryDataScript } from "./Scripts/get_category_data";
 import { getOwnedNFTs as getOwnedNFTsScript } from "./Scripts/get_owned_nfts";
 
 //
 // // ****** Script Functions ****** //
 //
 
-// Get list of Collection's Metadatas.
+// Get info on specific Platform ID.
 export const getPlatformData = async () => {
   try {
     const response = await fcl.query({
       cadence: getPlatformDataScript(),
+      args: (arg, t) => [arg("0", t.UInt64)],
+    });
+    console.log(response);
+    return response;
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// Get info on specific a Category ID.
+export const getCategoryData = async () => {
+  try {
+    const response = await fcl.query({
+      cadence: getCategoryDataScript(),
       args: (arg, t) => [arg("0", t.UInt64)],
     });
     console.log(response);
